4047fa07352fd94163d3066488e6f5fab7456c49
SujanBoopathy <80327356+SujanBoopathy@users.noreply.github.com>
2022-10-12 20:35:53 +0530

    java-coding

--- classes/MemberInnerClass.java
+++ classes/MemberInnerClass.java
@@ -3,14 +3,14 @@ public class MemberInnerClass{
 
 	class InnerClass{
 		void display(){
-			System.out.println("Data value:"+data);
+			System.out.println("Data value is :"+data);
 		}
 	}
 
 
 	public static void main(String args[]){
-		MemberInnerClass obj=new MemberInnerClass();
-		MemberInnerClass.InnerClass inn=obj.new InnerClass();
-		inn.display();
+		MemberInnerClass object=new MemberInnerClass();
+		MemberInnerClass.InnerClass innerClassObject=object.new InnerClass();
+		innerClassObject.display();
 	}
 }
